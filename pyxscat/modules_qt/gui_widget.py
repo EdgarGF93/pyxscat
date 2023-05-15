b1b7668294de6013d7235d0af6c69ec30daca575
edgar1993a <edgar.gutierrez-fernandez@esrf.fr>
2023-05-15 12:13:10 +0200

    live

--- pyxscat/modules_qt/gui_widget.py
+++ pyxscat/modules_qt/gui_widget.py
@@ -1090,7 +1090,10 @@ class GUIPyX_Widget(GUIPyX_Widget_layout):
                     list_files_to_display = new_files
                 else:
                     reset = True
+                    self.clicked_folder = folder_last_file
                     list_files_to_display = self._dict_files[folder_last_file]
+
+                print(list_files_to_display)
 
                 self.update_table(
                     list_files=list_files_to_display,
