matrix rotation fixed

pyxscat/h5_integrator.py
@@ -1960,53 +1960,57 @@ class H5Integrator(Transform):
             logger.info(f"Data is None. Returns.")
             return
 
-        # Get the detector array
-        det_array = self.get_detector_array(shape=data.shape)
+        # Get the detector array, it is always the same shape (RAW MATRIX SHAPE!), no rotations yet
+        shape = data.shape
+        det_array = self.get_detector_array(shape=shape)
 
         # Get the mesh matrix
         if unit in UNITS_Q:
             try:
-                scat_z, scat_x = self.calc_q(
+                # calc_q will take into account the sample_orientation in GrazingGeometry instance
+                scat_z, scat_xy = self.calc_q(
                     d1=det_array[0,:,:],
                     d2=det_array[1,:,:],
                 )
                 logger.info(f"Shape of the scat_z matrix: {scat_z.shape}")
-                logger.info(f"Shape of the scat_x matrix: {scat_x.shape}")
+                logger.info(f"Shape of the scat_x matrix: {scat_xy.shape}")
             except:
-                scat_z, scat_x = None, None
+                scat_z, scat_xy = None, None
                 logger.info(f"Scat_z matrix could not be generated.")
                 logger.info(f"Scat_x matrix could not be generated.")
+
         elif unit in UNITS_THETA:
             try:
-                scat_z, scat_x = self.calc_angles(
+                scat_z, scat_xy = self.calc_angles(
                     d1=det_array[0,:,:],
                     d2=det_array[1,:,:],
                 )
                 logger.info(f"Shape of the scat_z matrix: {scat_z.shape}")
-                logger.info(f"Shape of the scat_x matrix: {scat_x.shape}")
+                logger.info(f"Shape of the scat_x matrix: {scat_xy.shape}")
             except:
-                scat_z, scat_x = None, None
+                scat_z, scat_xy = None, None
                 logger.info(f"Scat_z matrix could not be generated.")
                 logger.info(f"Scat_x matrix could not be generated.")
 
         # Transform units
-        if (scat_z is not None) and (scat_x is not None):
+        if (scat_z is not None) and (scat_xy is not None):
             DICT_PLOT = DICT_UNIT_PLOTS.get(unit, DICT_PLOT_DEFAULT)
             scat_z *= DICT_PLOT['SCALE']
-            scat_x *= DICT_PLOT['SCALE']
+            scat_xy *= DICT_PLOT['SCALE']
             logger.info(f"Changing the scale of the matriz q units. Scale: {DICT_PLOT['SCALE']}")
 
+            # Defining the missing wedge
             if unit in UNITS_Q:
                 NUMBER_COLUMNS_REMOVED = 10
                 HALF_NUMBER = int(NUMBER_COLUMNS_REMOVED/2)
-                ind = np.unravel_index(np.argmin(abs(scat_x), axis=None), scat_z.shape)
+                ind = np.unravel_index(np.argmin(abs(scat_xy), axis=None), scat_z.shape)
                 if self.get_sample_orientation() in (1,3):
                     data[:, ind[1] - HALF_NUMBER: ind[1] + HALF_NUMBER] = np.nan
                 elif self.get_sample_orientation() in (2,4):
                     data[ind[0] - HALF_NUMBER: ind[0] + HALF_NUMBER, :] = np.nan
                 logger.info(f"The missing wedge was removed from the 2D map.")
 
-        return scat_x, scat_z, data
+        return scat_xy, scat_z, data
 
         # if self.tranform_bool:
         #     unit = get_pyfai_unit(unit)