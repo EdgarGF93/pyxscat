6d5ccd454bcac328c845ca647675a60616df1646
Edgar Gutierrez <edgargutierrezfdez@gmail.com>
2023-08-22 17:17:48 +0200

    icon logo

--- pyxscat/gui/gui_window.py
+++ pyxscat/gui/gui_window.py
@@ -1,18 +1,13 @@
 
-from . import GUI_PATH
-# from pyxscat.other.integrator_methods import DIRECTORY_INTEGRATIONS
-# from pyxscat.gui.gui_widget import GUIPyX_Widget
-# from pyxscat.gui.gui_widget_about import AboutForm
-
-# from other.integrator_methods import DIRECTORY_INTEGRATIONS
+from . import ICON_PATH
 from gui.gui_widget import GUIPyX_Widget
 from gui.gui_widget_about import AboutForm
 
 
 from PyQt5.QtWidgets import QMainWindow, QMenu, QMenuBar, QAction
 from PyQt5 import QtGui, QtCore
-from os.path import join
-import os
+
+PYXSCAT_LOGO = "pyxscat_icon.png"
 
 class GUIPyX_Window(QMainWindow):
     def __init__(self, *args):
@@ -23,7 +18,7 @@ class GUIPyX_Window(QMainWindow):
         self.setWindowTitle("PyXScat")
 
         app_icon = QtGui.QIcon()
-        icon_path = str(GUI_PATH.joinpath("pyxscat_icon.png"))
+        icon_path = str(ICON_PATH.joinpath(PYXSCAT_LOGO))
         app_icon.addFile(icon_path, QtCore.QSize(256,256))
         self.setWindowIcon(app_icon)
 
@@ -50,17 +45,3 @@ class GUIPyX_Window(QMainWindow):
     def _open_about_form(self):
         self.about_form = AboutForm()
         self.about_form.show()
-
-    # def get_dictionaries_integration(self) -> list:
-    #     """
-    #         Return a list with the dictionaries of all the available integrations
-    #     """
-    #     import json
-    #     list_dicts = []
-    #     for file in os.listdir(DIRECTORY_INTEGRATIONS):
-    #         if file.endswith('json'):
-    #             with open(join(DIRECTORY_INTEGRATIONS, file), 'r') as fp:
-    #                 list_dicts.append(
-    #                     json.load(fp)
-    #                 )
-    #     return list_dicts
